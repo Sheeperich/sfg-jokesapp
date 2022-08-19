2c167a7092088cd78ffdd1a827f57d4aa4152e5c
Emanuel <sheeperich@gmail.com>
2022-08-19 21:33:11 +0200

    Setup of Project

---
+++ JokesApp/src/main/java/de/sheeperich/sfg/jokesapp/JokesAppApplication.java
@@ -0,0 +1,13 @@
+package de.sheeperich.sfg.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesAppApplication.class, args);
+	}
+
+}
---
+++ JokesApp/src/test/java/de/sheeperich/sfg/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sheeperich.sfg.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
